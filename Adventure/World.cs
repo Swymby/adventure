4d871d664acbe2ee9c85097edf6335fbb80d3ab5
Jan Švimberský <jan.svimbersky@gmail.com>
2024-02-08 19:50:27 +0100

    initial

---
+++ Adventure/World.cs
@@ -0,0 +1,24 @@
+﻿using Adventure.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure
+{
+    public class World
+    {
+        public BaseRoom CurrentRoom { get; private set; }
+
+        public World() 
+        {
+            CurrentRoom = new Lobby();
+        }
+
+        public string Intro { get; } = @"
+How did you end up here? You don't really know. Maybe you fell, maybe someone pushed you.
+One thing is certain, you need to GET OUT OF HERE!
+";
+    }
+}
